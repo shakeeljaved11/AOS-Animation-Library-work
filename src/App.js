f546875e5d34619ca96aa493f292238ea0bce862
Muneeb732 <chaudharymuneeb732@gmail.com>
2024-07-24 11:45:42 +0500

    muneeb

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
+import MuneebWork from './components/MuneebWork';
 
 function App() {
   return (
     <div className="App">
    <HomePage/>
+   <MuneebWork/>
     </div>
   );
 }
---
+++ src/components/MuneebWork.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MuneebWork = () => {
+  return (
+    <div>
+      <div 
+      
+      style={{backgroundColor:"purple",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+      <div 
+      data-aos="fade-zoom-in"
+      data-aos-duration="3000"
+      style={{backgroundColor:"navy",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+    </div>
+  )
+}
+
+export default MuneebWork;
