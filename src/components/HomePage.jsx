b7ea132c50368d4e12fc0f36eadfc7531b61b041
MuhammadAzam11 <muhammdazam8i@gmail.com>
2024-07-24 11:46:10 +0500

    two

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,8 @@ const HomePage = () => {
         // Aos.init({duration:2000})
     },[])
   return (
-    <div>
+    <>
+    <div className='main'>
     <div 
     data-aos="fade-right"
     data-aos-duration="1500"
@@ -21,12 +22,22 @@ const HomePage = () => {
     <div
     data-aos="flip-left"
     style={{backgroundColor:"blue",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
-    <div data-aos="zoom-in" style={{backgroundColor:"yellow",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
-    <div style={{backgroundColor:"pink",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
-    <div style={{backgroundColor:"orange",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
-    <div style={{backgroundColor:"aqua",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
-    <div style={{backgroundColor:"brown",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+    <div data-aos="zoom-in"   data-aos-duration="1500"  style={{backgroundColor:"yellow",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+    <div data-aos="zoom-out-down"   data-aos-duration="1500"  style={{backgroundColor:"pink",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+    <div data-aos="zoom-out-right"   data-aos-duration="1500"  style={{backgroundColor:"orange",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+    <div data-aos="zoom-out-left"   data-aos-duration="1500"  style={{backgroundColor:"aqua",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+    <div  data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500" 
+    style={{backgroundColor:"brown",width:"300px",height:"200px",margin:"30px"}}>Animation On Scroll</div>
+    
     </div>
+    <div data-aos="fade-left"
+     data-aos-anchor="#example-anchor"
+     data-aos-offset="500"
+     data-aos-duration="500">
+</div>
+    </>
   )
 }
 
